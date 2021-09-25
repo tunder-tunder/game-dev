7d0101fc3290ea04ed888e199bd0a29cde30f081
Alina Shcherbinina <alinkash@gmail.com>
2021-09-25 11:04:11 +0800

    friction homework in progress

--- friction/main.lua
+++ friction/main.lua
@@ -5,65 +5,69 @@ function love.load()
     width = love.graphics.getWidth()
     height = love.graphics.getHeight()
     love.graphics.setBackgroundColor(128 / 255, 128 / 255, 128 / 255)
-    --location = Vector:create(width / 2, height / 2)
-    location1 = Vector:create(width / 4, height / 2)
-    location2 = Vector:create(width / 4 * 3, height / 2)
+    
+    location = Vector:create(100, height/2)
+    wlocation = Vector:create(500, height/2)
     velocity = Vector:create(0, 0)
-    mover = Mover:create(location1, velocity)
-    wmover = Mover:create(location2, velocity, 5)
+    
+    mover = Mover:create(location, velocity)
+    mover.size = 20
+    wmover = Mover:create(wlocation, velocity, 5)
     wmover.size = 20
+
+    -- create rects somehow 
+
+    -- rectangleBlue = 
+    -- rectangleGreen =
 
     wind = Vector:create(0.01, 0)
     isWind = false
+    
     gravity = Vector:create(0, 0.01)
     isGravity = false
+    
     floating = Vector:create(0, -0.02)
     isFloating = false
 end
 
 function love.update()
-    mover:applyForce(gravity)
-    --mover:applyForce(wind)
-    wmover:applyForce(gravity)
-    --wmover:applyForce(wind)
 
-    if (mover.location.x < width / 2) then
+    
+    if isGravity then
+        mover:applyForce(gravity)
+        wmover:applyForce(gravity)
+    end
+
+    if isFloating then 
+        mover:applyForce(floating)
+        wmover:applyForce(floating)
+    end
+
+    if isWind then 
+        mover:applyForce(wind)
+        wmover:applyForce(wind)
+    end
+
+
+    if (mover.location.x + mover.size < 100 and mover.location.y + mover.size < 100) then
         friction = (mover.velocity * -1):norm()
         if friction then
-            friction:mul(0.005)
+            friction:mul(0.05)
             mover:applyForce(friction)
         end
     end
 
-    if (wmover.location.x < width / 2) then
+    if (wmover.location.x + wmover.size < 400 and wmover.location.y + wmover.size < 100 ) then
         friction = wmover.velocity:norm()
         if friction then
-            friction:mul(0.005)
+            friction:mul(-0.05)
             wmover:applyForce(friction)
         end
     end
     
-    -- if isGravity then
-    --     mover:applyForce(gravity)
-    -- end
-    -- if isWind then
-    --     mover:applyForce(wind)
-    -- end
-    -- if isFloating then
-    --     mover:applyForce(floating)
-    -- end
     mover:update()
     mover:checkBoundaries()
-
-    -- if isGravity then
-    --     wmover:applyForce(gravity)
-    -- end
-    -- if isWind then
-    --     wmover:applyForce(wind)
-    -- end
-    -- if isFloating then
-    --     wmover:applyForce(floating)
-    -- end
+    
     wmover:update()
     wmover:checkBoundaries()
 end
@@ -71,32 +75,36 @@ end
 function love.draw()
     mover:draw()
     wmover:draw()
+    
     love.graphics.print(tostring(mover.velocity), mover.location.x + mover.size, mover.location.y)
     love.graphics.print(tostring(wmover.velocity), wmover.location.x + wmover.size, wmover.location.y)
     love.graphics.print("w: " .. tostring(isWind) .. " g: " .. tostring(isGravity) .. " f: " .. tostring(isFloating))
     
-    local r,g,b,a = love.graphics.getColor()
-    love.graphics.setColor(0, 119/255, 119/255, 0.5)
-    love.graphics.rectangle("fill", 0, 0, 400, 800)
-    love.graphics.setColor(r,g,b,a)
+    local r, g, b, a = love.graphics.getColor()
+	love.graphics.setColor(0, 199/255, 90/255, 0.5)
+	love.graphics.rectangle("fill", 100, 100, 200, 200)
+	
+    love.graphics.setColor(0, 230/255, 52/255, 0.5)
+	love.graphics.rectangle("fill", 400, 100, 200, 200)
+    love.graphics.setColor(r, g, b, a)
 
-    local r,g,b,a = love.graphics.getColor()
-    love.graphics.setColor(119/255, 0, 0, 0.5)
-    love.graphics.rectangle("fill", width/2, 0, 400, 800)
-    love.graphics.setColor(r,g,b,a)
 end
 
 function love.keypressed(key)
+    
     if key == 'g' then
         isGravity = not isGravity
     end
+    
     if key == 'f' then
         isFloating = not isFloating
     end
+    
     if key == 'w' then
         isWind = not isWind
         if isWind then
             wind = wind * -1
         end
     end
+
 end
